7435648c1902c1d448ea216e298e28973ac7f472
KobeLemon <kobe42lemon@outlook.com>
2025-05-26 12:04:52 -0600

    Updated Repair model

--- MechMate/Models/RepairInStance.cs
+++ MechMate/Models/RepairInStance.cs
@@ -1,12 +1,18 @@
-﻿namespace MechMate.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MechMate.Models
 {
     class RepairInstance
     {
-        public int RepairId { get; set; }
-        public string VehicleId { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string RepairId { get; set; } = ObjectId.GenerateNewId().ToString();
+        public string VehicleId { get; set; } = string.Empty;
         public DateTime RepairDate { get; set; }
-        public Dictionary<string, string> RepairNotes { get; set; }
+        public Dictionary<string, string> RepairNotes { get; set; } = new Dictionary<string, string>();
         public int RepairMileage { get; set; }
-        public List<string> ToolsUsed { get; set; }
+        public List<string> ToolsUsed { get; set; } = new List<string>();
+        public List<RepairPart> PartsUsed { get; set; } = new List<RepairPart>();
     }
 }
