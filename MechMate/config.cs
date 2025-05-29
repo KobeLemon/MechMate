f7e9c4794a0addeb84c1aebb4a39ebec57992e9c
dofelton <dofelton@gmail.com>
2025-05-28 20:07:00 -0500

    Database service

---
+++ MechMate/config.cs
@@ -0,0 +1,6 @@
+public static class MongoDbConfig
+{
+    public const string ConnectionString = "mongodb+srv://user1:<mechmate>@mechmate.sxdkq8x.mongodb.net/?retryWrites=true&w=majority&appName=MechMate";
+    public const string DatabaseName = "MechMateDB";
+    public const string CollectionName = "MyGarage"; 
+}
