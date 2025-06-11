4308eb4ef49b0b892ee2ffddb45241f8d61ff401
dofelton <dofelton@gmail.com>
2025-06-11 12:09:39 -0500

    update variables

--- MechMate/MondoDBServices.cs
+++ MechMate/MondoDBServices.cs
@@ -1,18 +1,20 @@
 ﻿using MongoDB.Driver;
 using MechMate.Models;
 using MongoDB.Driver.Core.Configuration;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MechMate.Services
 {
     public class MongoDbService
     {
-        private readonly IMongoCollection<MyGarage> _MyGarage;
+        private readonly IMongoCollection<Vehicle> _vehicles;
 
-        public MongoDbService(string config.ConnectionString, string config.databaseName)
+        public MongoDbService(string connectionString, string databaseName)
         {
             var client = new MongoClient(connectionString);
             var database = client.GetDatabase(databaseName);
-            _vehicles = database.GetCollection<MyGarage>("MyGarage");
+            _vehicles = database.GetCollection<Vehicle>("MyGarage");
         }
 
         public async Task<List<Vehicle>> GetVehiclesAsync()
