c88517bbdb65b3ff0fdeb7f8ee75fa8f1b08f1cd
dofelton <dofelton@gmail.com>
2025-06-11 20:44:36 -0500

    Increase database connection timeout

--- MechMate/config.cs
+++ MechMate/config.cs
@@ -1,6 +1,6 @@
 public static class MongoDbConfig
 {
-    public const string ConnectionString = "mongodb+srv://user1:<mechmate>@mechmate.sxdkq8x.mongodb.net/?retryWrites=true&w=majority&appName=MechMate";
+    public const string ConnectionString = "mongodb+srv://user1:<mechmate>@mechmate.sxdkq8x.mongodb.net/?connectTimeoutMS=60000&retryWrites=true&w=majority&appName=MechMate";
     public const string DatabaseName = "MechMateDB";
     public const string CollectionName = "MyGarage"; 
 }
